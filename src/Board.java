e3f4d3c60bd95bf82856778fef02ca9fa7f7017c
oTriks <martin.x.larsson.ml@gmail.com>
2023-10-04 20:13:44 +0200

    gameboard menu

--- src/Board.java
+++ src/Board.java
@@ -2,16 +2,26 @@ import java.util.ArrayList;
 
 public class Board {
 
-    ArrayList<String> board = new ArrayList<>(9);
+    private ArrayList<String> board = new ArrayList<>(9);
 
 
     public Board() {
-
+for(int i = 0; i < 9; i++) {
+    board.add("   ");
+}
     }
 
     public void printBoard() {
-    for(int i = 0; i < board.size(); i++) {
-        System.out.print(board.get(i));
+    for(int i = 0; i < this.board.size(); i++) {
+
+        System.out.print(this.board.get(i));
+        if (i != 2 && i != 5 && i != 8){
+
+            System.out.print("|");
+        }else {
+
+            System.out.println("\n---+---+---");
+        }
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int choice;
+        Board board = new Board();
 
         do {
             System.out.println("Välkommen, välj spelläge:\n 1. Spela mot en vän\n 2. Spela mot datorn \n 3. Avsluta");
             choice = sc.nextInt();
-            switch (choice) {
+    sc.nextLine();
+                switch (choice) {
                 case 1:
                     System.out.println("Skriv in första spelarens namn");
                     String player1 = sc.nextLine();
                     System.out.println("Skriv in andra spelarens namn");
                     String player2 = sc.nextLine();
+                    board.printBoard();
+                    break;
 
                 case 2:
                     System.out.println("Skriv in ditt namn");
                     String player = sc.nextLine();
+
+                    break;
+
                 case 3:
                     System.out.println("Hejdå!");
                     break;
